e9a03b2168e591afa2314d18b03d9a7ee5c1c2a5
xuefeng <jerrypeng777@yeah.net>
2016-11-17 16:51:35 -0500

    separate the main code to several functions

--- data preprocessing scripts/trip_processing.py
+++ data preprocessing scripts/trip_processing.py
@@ -7,7 +7,8 @@ weather_dict = json.loads(open('weather_processed.json','r').read())
 region_dict = {}
 
 offset = 0
-link = 'https://data.cityofnewyork.us/resource/gkne-dk5s.json?$limit=2&' + '$offset=' + str(offset)
+limit = '500'
+link = 'https://data.cityofnewyork.us/resource/gkne-dk5s.json?$limit='+ limit +'&$offset=' + str(offset)
 # output = open('trips_processed.json', 'a+')
 
 ##### MAIN FUNCTIONS
@@ -15,6 +16,7 @@ link = 'https://data.cityofnewyork.us/resource/gkne-dk5s.json?$limit=2&' + '$off
 def query_trip(link):
 	response = urllib2.urlopen(link)
 	return json.loads(response.read())
+
 # take a trip and 
 # 1. get pick_up/drop_off geo-locations
 # 	 get time of trip, passenger #, 
@@ -23,28 +25,17 @@ def query_trip(link):
 # 3. write json object into file
 def parse_trip(trip):
 
-	### get basic info of trip
+	### get basic info of trip (time, coord, passenger)
 	pick_up_time = trip['pickup_datetime']
 	pick_up_coord = {'lat':trip['pickup_latitude'], 'log': trip['pickup_longitude']}
 	drop_off_coord = {'lat':trip['dropoff_latitude'], 'log': trip['dropoff_longitude']}
 	passenger = trip['passenger_count']
 
-	### get the weather info of the trip
-	# process pick_up_time in the corresponding format to search weather
-	time_arr = (pick_up_time.split("T")[0]).split('-')
-	year = time_arr[0]
-	month = time_arr[1]
-	date = time_arr[2]
-	# the key for searching weather in weather dict
-	year_search_key = year + '/' + month + '/' + date
-	hour_search_point = (pick_up_time.split("T")[1]).split(':')[0]
-	if hour_search_point[0] == '0': #trim the leading 0 (i.e 07 -> 7)
-		hour_search_point = hour_search_point[1]
-	print hour_search_point
-	temp = weather_dict[year_search_key][hour_search_point]['temp']
-	condition = weather_dict[year_search_key][hour_search_point]['condition']
+	### get the weather info of the trip (temp, condition)
+	temp = get_weather_info(pick_up_time)[0]
+	condition = get_weather_info(pick_up_time)[1]
 	
-	### get the cluster region info of the trip
+	### get the cluster region info of the trip (region)
 
 	# encapsulation
 	trip = {
@@ -59,6 +50,26 @@ def parse_trip(trip):
 	return trip
 
 ##### AUXULIARY FUNCTIONS
+def get_weather_info(time):
+	# process pick_up_time in the corresponding format to search weather
+	time_arr = (pick_up_time.split("T")[0]).split('-')
+	year = time_arr[0]
+	month = time_arr[1]
+	date = time_arr[2]
+	# the key for searching weather in weather dict
+	year_search_key = year + '/' + month + '/' + date
+	hour_search_point = (pick_up_time.split("T")[1]).split(':')[0]
+	
+	# trim the leading 0 (i.e 07 -> 7)
+	if hour_search_point[0] == '0':
+		hour_search_point = hour_search_point[1]
+
+	temp = weather_dict[year_search_key][hour_search_point]['temp']
+	condition = weather_dict[year_search_key][hour_search_point]['condition']
+	return [temp, condition]
+
+# def get_region_info(coord):
+
 # def get_distance(log_1, lat_1, log_2, lat_2):
 
 
