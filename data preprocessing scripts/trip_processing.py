85d46cde5eb6c8036f9f42b0d5564e09a614240d
xuefeng <jerrypeng777@yeah.net>
2016-11-17 18:57:35 -0500

    1. Added weekday

--- data preprocessing scripts/trip_processing.py
+++ data preprocessing scripts/trip_processing.py
@@ -1,5 +1,7 @@
 import urllib2
 import json
+import datetime
+import calendar
 
 # load popular cluster dictionary
 weather_dict = json.loads(open('weather_processed.json','r').read())
@@ -31,15 +33,28 @@ def parse_trip(trip):
 	drop_off_coord = {'lat':trip['dropoff_latitude'], 'log': trip['dropoff_longitude']}
 	passenger = trip['passenger_count']
 
+	## get the time component
+	# process pick_up_time in the corresponding format to search weather
+	time_arr = (pick_up_time.split("T")[0]).split('-')
+	year = time_arr[0]
+	month = time_arr[1]
+	date = time_arr[2]
+
+	# get weekday
+	dateObj = datetime.datetime(int(year),int(month), int(date))
+	weekday = dateObj.weekday()
+
 	### get the weather info of the trip (temp, condition)
-	temp = get_weather_info(pick_up_time)[0]
-	condition = get_weather_info(pick_up_time)[1]
+	weather_info = get_weather_info(year, month, date)
+	temp = weather_info[0]
+	condition = weather_info[1]
 	
 	### get the cluster region info of the trip (region)
 
 	# encapsulation
 	trip = {
-				'pick_up_time': pick_up_time, 
+				'pick_up_time': pick_up_time,
+				'weekday': weekday,
 				'pick_up_coord': pick_up_coord, 
 				'drop_off_coord': drop_off_coord,
 				'passenger_count': passenger,
@@ -50,12 +65,8 @@ def parse_trip(trip):
 	return trip
 
 ##### AUXULIARY FUNCTIONS
-def get_weather_info(time):
-	# process pick_up_time in the corresponding format to search weather
-	time_arr = (pick_up_time.split("T")[0]).split('-')
-	year = time_arr[0]
-	month = time_arr[1]
-	date = time_arr[2]
+def get_weather_info(year, month, date):
+	
 	# the key for searching weather in weather dict
 	year_search_key = year + '/' + month + '/' + date
 	hour_search_point = (pick_up_time.split("T")[1]).split(':')[0]
@@ -75,5 +86,6 @@ def get_weather_info(time):
 
 for trip in query_trip(link):
 	print parse_trip(trip)
+
 
 
