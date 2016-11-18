553004cca4a134c41e4fc920f9d74e679c93aa54
xuefeng <jerrypeng777@yeah.net>
2016-11-18 16:11:40 -0500

    1. The latest weather.json

--- data src/color.py
+++ data src/color.py
@@ -1,14 +1,27 @@
 import json
 
-output = open('weather_processed.json', 'w')
-weathers = {}
-with open('weather_processed.txt') as f:
-    for line in f:
-      jobj = json.loads(line)
-      key = jobj.keys()[0]
-      weathers[key] =  jobj[key]
+weathers = json.loads(open('weather_processed.json').read())
+conditions = {}
+count = 0
+for key, value in weathers.items():
+	count += 1
+	for item in value.items():
+		condition = item[1]['condition']
+		if condition in conditions.keys():
+			conditions[condition] += 1
+		else:
+			conditions[condition] = 1
 
-json.dump(weathers, output)
+print conditions
+# output = open('weather_processed.json', 'w+')
+# weathers = {}
+# with open('weather_processed.txt') as f:
+#     for line in f:
+#       jobj = json.loads(line)
+#       key = jobj.keys()[0]
+#       weathers[key] =  jobj[key]
+
+# json.dump(weathers, output)
         
 
 
