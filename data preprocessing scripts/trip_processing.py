c7b2db131396380219b624a595a5024e1084523a
xuefeng <jerrypeng777@yeah.net>
2016-11-18 17:53:24 -0500

    1. Improved the speed of trip processing 2. Print debug log

--- data preprocessing scripts/trip_processing.py
+++ data preprocessing scripts/trip_processing.py
@@ -3,15 +3,29 @@ import json
 from math import cos,asin,sqrt
 import datetime
 import calendar
+import logging
+import time
 
-
+logging.basicConfig(filename = "debug.log", level = logging.DEBUG)
 # load popular cluster dictionary
+region_dict = json.loads(open('Clusters.json','r').read())
+# load region 
 weather_dict = json.loads(open('weather_processed.json','r').read())
+
+# condition category
+condition_dict = {
+	"normal": ['ScatteredClouds', 'MostlyCloudy', 'PartlyCloudy', 'Clear', 'Overcast', 'Haze', 'Mild', 'Quitecool', 'Chilly', 'Frigid', 'Cold', 'Cool'],
+	"heavy_rain": ['Rain', 'HeavyRain'],
+	"light_rain": ['LightFreezingRain', 'LightRain'],
+	"heavy_snow": ['HeavySnow', 'Snow'],
+	"light_snow": ['LightSnow'],
+	"fog": ['Fog', 'Mist'],
+}
 
 offset = 0
 limit = '100'
 link = 'https://data.cityofnewyork.us/resource/gkne-dk5s.json?$limit='+ limit +'&$offset=' + str(offset)
-output = open('processed_trips.json', 'a+')
+output = open('processed_trips.txt', 'a+')
 
 ##### MAIN FUNCTIONS
 
@@ -35,7 +49,9 @@ def parse_trip(trip):
 	passenger = trip['passenger_count']
 
 	### get the cluster region info of the trip (region)
+	start_time = time.time()
 	region_id = get_region_info(trip, pick_up_coord)
+	print "get region_id costs " + str(time.time() - start_time) + " s"
 	if region_id == 0:
 		# trip far fram all popular regions, ignore it
 		return
@@ -51,19 +67,24 @@ def parse_trip(trip):
 	date = time_arr[2]
 	if date[0] == '0':
 		date = date[1]
+	hour = (pick_up_time.split("T")[1]).split(':')[0]
+	# trim the leading 0 (i.e 07 -> 7)
+	if hour[0] == '0':
+		hour = hour[1]
 
 	# get weekday
 	dateObj = datetime.datetime(int(year),int(month), int(date))
 	weekday = dateObj.weekday()
 
 	### get the weather info of the trip (temp, condition)
-	weather_info = get_weather_info(pick_up_time, year, month, date)
+	weather_info = get_weather_info(pick_up_time, year, month, date, hour)
 	temp = weather_info[0]
-	condition = weather_info[1]
+	condition = get_condition_label(weather_info[1])
 	
 	# encapsulation
 	trip = {
-				'pick_up_time': pick_up_time,
+				'pick_up_date': month + '/' + date,
+				'pick_up_hour': hour,
 				'weekday': (weekday + 1),
 				'pick_up_coord': pick_up_coord, 
 				'drop_off_coord': drop_off_coord,
@@ -84,12 +105,12 @@ def get_region_info(trip, coord):
 	upper_log = float(trip['pickup_longitude']) + 1 / (111.320 * cos(p*float(trip['pickup_latitude'])))
 	
 	# search for the nearest region
-	with open('Clusters.json','r') as input:
-		clusters = json.load(input)
-		for each in clusters:
-			if float(str(each['coordinates'][0])) < lower_log or float(str(each['coordinates'][0])) > upper_log or float(str(each['coordinates'][1])) < lower_lat or float(str(each['coordinates'][1])) > upper_lat:
-				#print each['coordinates'][0]
-				clusters.remove(each)
+	# shallow copy
+	clusters = []
+	for each in region_dict:
+		if not (float(str(each['coordinates'][0])) < lower_log or float(str(each['coordinates'][0])) > upper_log or float(str(each['coordinates'][1])) < lower_lat or float(str(each['coordinates'][1])) > upper_lat):
+			#print each['coordinates'][0]
+			clusters.append(each)
 
 	min = 1138
 	index = 0
@@ -100,18 +121,21 @@ def get_region_info(trip, coord):
 			index = each['index']
 	return index
 
-def get_weather_info(pick_up_time, year, month, date):
+def get_weather_info(pick_up_time, year, month, date, hour):
+	
 	# the key for searching weather in weather dict
 	year_search_key = year + '/' + month + '/' + date
-	hour_search_point = (pick_up_time.split("T")[1]).split(':')[0]
 	
-	# trim the leading 0 (i.e 07 -> 7)
-	if hour_search_point[0] == '0':
-		hour_search_point = hour_search_point[1]
 
-	temp = weather_dict[year_search_key][hour_search_point]['temp']
-	condition = weather_dict[year_search_key][hour_search_point]['condition']
+	temp = weather_dict[year_search_key][hour]['temp']
+	condition = weather_dict[year_search_key][hour]['condition']
 	return [temp, condition]
+
+# convert raw condition to condition label
+def get_condition_label(condition):
+	for key in condition_dict.keys():
+		if condition in condition_dict[key]:
+			return key
 
 # given two coordinate, comput their distance
 def distance(lat1, lon1, lat2, lon2):
@@ -119,21 +143,38 @@ def distance(lat1, lon1, lat2, lon2):
 	a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
 	return (12742 * asin(sqrt(a)))
 
-offset_upper_bound = 1651
-processed_trip_count = 0
+
+##### MAIN QUERY LOOPS
+
+offset_upper_bound = 1
+cp_processed_trip_count = 0 # current page processed count
 skip = 0
+
 while offset < offset_upper_bound:
-	for trip in query_trip(link):
-		processed_trip_count += 1
-		if processed_trip_count > skip:
-			print "processing trip." + str(processed_trip_count) + "at page." + str(offset)
+	
+	start_time = time.time()
+	trips = query_trip(link)
+	print "query costs " + str(time.time() - start_time) + " s"
+
+	start_time = time.time()
+	for trip in trips:
+		cp_processed_trip_count += 1
+		if cp_processed_trip_count > skip:
+			#print "processing trip no." + str(cp_processed_trip_count) + " at page." + str(offset)
+			#print trip
+			#print parse_trip(trip)
 			try:
 				if parse_trip(trip) is not None:
-				# dump to json output
-				trip_obj = parse_trip(trip)
-				json.dumps(trip_obj, output)
+					# dump to json output
+					trip_obj = parse_trip(trip)
+					output.write(json.dumps(trip_obj) + '\n')
 			except Exception as e:
 				# write error message and trip index to error file
-				continue
+				errMsg = "processing trip no." + str(cp_processed_trip_count) + " at page." + str(offset) + ' failed: ' + str(e)
+				logging.debug(errMsg)
+				pass
+	print "process costs " + str(time.time() - start_time) + "s"
+	offset += 1
+	cp_processed_trip_count = 0
 
 print "trip processing is completed"
